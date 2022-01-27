7b924a1292a459def650249227b0787ec8a676d6
yacine-maghezzi-sonarsource <91723921+yacine-maghezzi-sonarsource@users.noreply.github.com>
2022-01-27 16:54:28 +0100

    Added Vulnerability

--- src/NzbDrone.Integration.Test/GenericApiFixture.cs
+++ src/NzbDrone.Integration.Test/GenericApiFixture.cs
@@ -43,5 +43,62 @@ namespace NzbDrone.Integration.Test
 
             response.StatusCode.Should().Be(HttpStatusCode.NotAcceptable);
         }
+        public void should_get_unacceptable_with_accept_header(string header)
+        {
+            var sql = @"Update [User] SET FirstName = @FirstName WHERE Id = @Id";
+            context.Database.ExecuteSqlCommand(
+                sql,
+                new SqlParameter("@FirstName", firstname),
+                new SqlParameter("@Id", id));
+            //User input
+            string ipAddress = "127.0.0.1";
+
+            //check to make sure an ip address was provided
+            if (!string.IsNullOrEmpty(ipAddress))
+            {
+                // Create an instance of IPAddress for the specified address string (in
+                // dotted-quad, or colon-hexadecimal notation).
+                if (IPAddress.TryParse(ipAddress, out var address))
+                {
+                    // Display the address in standard notation.
+                    return address.ToString(); åß
+                }
+                services.Configure<IdentityOptions>(options =>
+{
+    // Password settings
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequiredUniqueChars = 6;
+
+
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+    options.Lockout.MaxFailedAccessAttempts = 3;
+
+    options.SignIn.RequireConfirmedEmail = true;
+
+    options.User.RequireUniqueEmail = true;
+});
+
+                //startup.cs
+                services.ConfigureApplicationCookie(options =>
+                {
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.Expiration = TimeSpan.FromHours(1)
+
+
+                 options.SlidingExpiration = true;
+                });
+
+
+            }
+
+        }
+
+
+
+
     }
 }
